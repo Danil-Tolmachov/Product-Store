09ee78a0fb787a67533b680e8b78fefc15e4c34a
Danil Tolmachov <danil.tolmachov45@gmail.com>
2024-03-26 15:45:08 +0200

    Update nav-bar component.

--- productsApp/src/app/nav-bar/nav-bar.component.ts
+++ productsApp/src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [ RouterLink, RouterLinkActive ],
+  imports: [ RouterLink, RouterLinkActive, CommonModule ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
