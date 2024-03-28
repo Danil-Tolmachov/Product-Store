1f5c327837561386b3c74e28299c1bbd6885482c
Danil Tolmachov <danil.tolmachov45@gmail.com>
2024-03-28 11:58:24 +0200

    Add cart-item & footer components.

--- productsApp/src/app/app.component.ts
+++ productsApp/src/app/app.component.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from '../components/nav-bar/nav-bar.component';
 import { ProductItemBriefComponent } from '../components/product-list/product-item-brief/product-item-brief.component';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,
-            NavBarComponent, 
-            ProductItemBriefComponent ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet,
+              NavBarComponent,
+              ProductItemBriefComponent,
+              FooterComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Products Store';
+    title = 'Products Store';
 }
