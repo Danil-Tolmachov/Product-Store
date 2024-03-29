a1500e4370623806afc8d41d3425a17ff483f319
Danil Tolmachov <danil.tolmachov45@gmail.com>
2024-03-29 14:20:02 +0200

    Init WebApi application.

---
+++ ProductStoreApi/ProductStoreApi/Startup.cs
@@ -0,0 +1,41 @@
+﻿
+namespace ProductStoreApi
+{
+	public class Startup
+	{
+		public IConfiguration Configuration { get; }
+
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddEndpointsApiExplorer();
+			services.AddSwaggerGen();
+			services.AddControllers();
+		}
+
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			// Configure the HTTP request pipeline.
+			if (env.IsDevelopment())
+			{
+				app.UseSwagger();
+				app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseRouting();
+			app.UseAuthorization();
+
+			app.UseEndpoints(e =>
+				e.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Home}/{action=Index}/{id?}")
+			);
+		}
+	}
+}
