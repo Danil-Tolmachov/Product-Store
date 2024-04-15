e612dd8480b44aa88a0a2a7e7fde0f563c4e7936
Danil Tolmachov <danil.tolmachov45@gmail.com>
2024-04-15 12:46:39 +0300

    Update AuthOptions.

--- ProductStoreApi/ProductStoreApi/Authentication/AuthOptions.cs
+++ ProductStoreApi/ProductStoreApi/Authentication/AuthOptions.cs
@@ -5,8 +5,8 @@ namespace ProductStoreApi.Authentication
 {
 	public static class AuthOptions
 	{
-		public const string ISSUER = "ProductStoreServer";
-		public const string AUDIENCE = "ProductStoreClient";
+		public const string ISSUER = "https://localhost:7048/";
+		public const string AUDIENCE = "https://localhost:4200/";
 		const string KEY = "This is my temporary secret key, don't use me!!!";
 		public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
 			new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
